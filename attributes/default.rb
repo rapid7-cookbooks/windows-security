2fc9c5aedfce2b3dc62ba4fa2026d9e051ff2910
Erran Carey <erran_carey@rapid7.com>
2013-06-26 14:26:31 -0500

    Update attributes::default to a new attribute layout

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,7 +9,7 @@
 
 # TODO: Just use :enabled, :disabled, :unchanged
 # ASLR can be set to: 0, 1, -1
-default[:windows][:aslr] = 1
+default[:windows][:security][:aslr] = 1
 
 # DEP can be set to: AlwaysOff, AlwaysOn, OptIn, or OptOut
-default[:windows][:dep] = 'OptIn'
+default[:windows][:security][:dep] = 'OptIn'
