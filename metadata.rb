051df09b9ca611d941d686c4d823d0993fec428c
Erran Carey <erran_carey@rapid7.com>
2013-06-26 14:18:09 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'rapid7'
+name             'windows-security'
 maintainer       'Rapid7'
 maintainer_email 'erran_carey@rapid7.com'
 license          'Copyright (c) 2012-2013, Rapid7. All Rights Reserved.'
